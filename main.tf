6d65e8a5f58cb729c632ee9114f408bd19f60f9f
MLyons <81724942+MsMLyons@users.noreply.github.com>
2022-11-05 08:47:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,4 +56,17 @@ resource "azurerm_subnet" "test-subnet" {
 }
 
 // run terraform plan, then apply
+    
+// create security group
+resource "azurerm_network_security_group" "test-sg" {
+    name = "test-security-group"
+    location = azurerm_resource_group.test-rg.location
+    resource_group_name = azurerm_resource_group.test-rg.name
+
+    tags = {
+        environment = "dev"
+    }
+}
+
+
 
