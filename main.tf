0e5f7502e5f95ff8f014acfd83cd59fcacf66c59
MLyons <81724942+MsMLyons@users.noreply.github.com>
2022-11-05 19:33:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "azurerm_linux_virtual_machine" "test-linuxVM" {
     }
     
     provisioner "local-exec" {
-        command = templatefile("windows-ssh-script.tpl", {
+        command = templatefile("${var.host_os}-ssh-script.tpl", {
             hostname = self.public_ip_address,
             user = "admin-user",
             identityfile = "~/.ssh/testazurekey"
